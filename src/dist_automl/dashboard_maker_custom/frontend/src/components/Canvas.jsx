d22a8e6f021d1de20cf3008d4aeb89afd599ce8e
legit-os <himanshu2005sanjay@gmail.com>
2026-04-17 18:38:00 +0530

    zoom fix

--- src/dist_automl/dashboard_maker_custom/frontend/src/components/Canvas.jsx
+++ src/dist_automl/dashboard_maker_custom/frontend/src/components/Canvas.jsx
@@ -11,10 +11,10 @@ const Canvas = ({ widgets, updateWidgetLayout, apiBase }) => {
         maxScale={3}
         panning={{ excluded: ['drag-handle', 'widget-body'] }}
         wheel={{
-          disabled: true,
-          // smooth: false,
-          // smoothStep: 0.005,
-          // activationKeys: ['Control']
+          disabled: false,
+          smooth: true,
+          smoothStep: 0.002,
+          step: 0.05
         }}
       >
         <TransformComponent
